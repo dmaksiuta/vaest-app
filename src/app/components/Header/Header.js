a856c30241ac8f0829e880e464d5cdbca35f31e7
dmaksiuta <shutle369@gmail.com>
2023-07-19 16:24:26 +0300

    created header component

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,14 @@
+import '../Header/Header.scss'
+
+export default function Header() {
+  return (
+    
+      <div>
+        <p>
+          Header works!
+        </p>
+        
+      </div>
+    
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import styles from './page.module.scss'
+import Header from './components/Header/Header'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div>
+        <Header />
         <p>
           Hello world
         </p>
         
       </div>
+      
 
       
       
