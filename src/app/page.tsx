3c72a2a59a31685bad21d0e0b979c945fbcc01ce
dmaksiuta <shutle369@gmail.com>
2023-07-24 21:57:18 +0300

    set up swiper for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,35 +18,34 @@ export default function Home() {
     <main className={styles.main}>
       <div className="main-section">
         <div className="main-section__carousel">
-        <Swiper
-       modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-      spaceBetween={5}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide><img src="main-bg-1.jpg" alt="restaurant building"/></SwiperSlide>
-      <SwiperSlide><img src="main-bg-2.jpg" alt="restaurant building"/></SwiperSlide>
-      <SwiperSlide><img src="main-bg-3.jpg" alt="restaurant building"/></SwiperSlide>
-      
-    </Swiper>
-
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+            spaceBetween={5}
+            slidesPerView={1}
+            loop={true}
+            autoplay={{
+              delay: 1500,
+              disableOnInteraction: false,
+            }}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}      
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+              <SwiperSlide><img src="main-bg-1.jpg" alt="restaurant building"/></SwiperSlide>
+              <SwiperSlide><img src="main-bg-2.jpg" alt="restaurant building"/></SwiperSlide>
+              <SwiperSlide><img src="main-bg-3.jpg" alt="restaurant building"/></SwiperSlide>
+        
+          </Swiper>
         </div>
-      
-
-
-
-                    <div className="main-section__info">
-              <h3 className="main-section__title">Velkommen til projekt Væst ved Trend strand</h3>
-              <p className="desc main-section__desc"><span>Væst</span> navnet er inspireret af Vesthimmerland, solnedgangen mod vest samt det nye danske køkken.</p>
-              <p className="text-14 main-section__desc">Vi ønsker at skabe et mødested med en bedre dansk restaurantoplevelse, for Trend sommerhusområde og dets beboere samt dagsgæster, til synet af endeløse skiftende solnedgange over Limfjorden ved den smukke Trend strand med øerne Fyr og Livø i baggrunden.</p>
-              <p className="text-14 main-section__desc">Projektet er beliggende på Gl. Amtsvej 39, Trend, 9670 Løgstør. Der hvor den gamle købmand blev bygget tilbage i 1949.</p>
-              <Link href="/baggrund" className="btn main-section__btn">Læs mere om projektet</Link>
-          </div>                                
+        <div className="main-section__info">
+            <h3 className="main-section__title">Velkommen til projekt Væst ved Trend strand</h3>
+            <p className="desc main-section__desc"><span>Væst</span> navnet er inspireret af Vesthimmerland, solnedgangen mod vest samt det nye danske køkken.</p>
+            <p className="text-14 main-section__desc">Vi ønsker at skabe et mødested med en bedre dansk restaurantoplevelse, for Trend sommerhusområde og dets beboere samt dagsgæster, til synet af endeløse skiftende solnedgange over Limfjorden ved den smukke Trend strand med øerne Fyr og Livø i baggrunden.</p>
+            <p className="text-14 main-section__desc">Projektet er beliggende på Gl. Amtsvej 39, Trend, 9670 Løgstør. Der hvor den gamle købmand blev bygget tilbage i 1949.</p>
+            <Link href="/baggrund" className="btn main-section__btn">Læs mere om projektet</Link>
+        </div>                                
       </div>
     </main>
   )
