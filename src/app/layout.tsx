de3ad4f9632a6747d19d0e51debc79745a9e1af7
dmaksiuta <shutle369@gmail.com>
2023-07-25 11:36:15 +0300

    fixed fonts and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,12 @@
 import './globals.scss';
 import type { Metadata } from 'next';
-import { Dancing_Script } from 'next/font/google';
 import Header from './@header/page';
 import Footer from './@footer/page';
 import Head from 'next/head';
 
-import localFont from 'next/font/local';
- 
-// Font files can be colocated inside of `app`
-const avenir = localFont({
-  src: './AvenirLT-Light.woff2',
-  variable: '--font-avenir',
-  display: 'swap',
-})
-
-
-const dancingScript = Dancing_Script({ 
-  subsets: ['latin'],
-  variable: '--font-dancing-script',
-  display: 'swap',
-   })
 
 export const metadata: Metadata = {
-  title: 'Restaurant | Væst | Løgstør',
-  
+  title: 'Restaurant | Væst | Løgstør',  
 }
 
 export default function RootLayout({
@@ -33,7 +16,7 @@ export default function RootLayout({
  
 }) {
   return (
-    <html lang="en" id='html' className={avenir.className}>
+    <html lang="en" id='html'>
       <Head>
           <link rel="apple-touch-icon" sizes="120x120" href="apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
